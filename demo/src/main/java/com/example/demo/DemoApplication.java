96919c455e9981c2f51c4d0663edb8f67718e742
Alnur <zholdas.alnur@gmail.com>
2024-09-09 13:22:56 +0500

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//	test comments from Alnur
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
