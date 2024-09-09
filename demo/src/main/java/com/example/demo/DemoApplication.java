849731c8993714c39ec8f9d37059612aa821509a
Dauren <dau.ibraimov12@gmail.com>
2024-09-09 21:24:53 +0500

    Loaded maven

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Ibraimov Dauren 
+//Ibraimov Dauren
 @SpringBootApplication
 public class DemoApplication {
 
