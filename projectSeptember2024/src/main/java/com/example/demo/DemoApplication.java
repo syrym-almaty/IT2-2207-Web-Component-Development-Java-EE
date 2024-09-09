c13fc8d04ffd39a8e436de56a50925649374c907
Abdyrassulov_Dinmukhammet <abd944@mail.ru>
2024-09-09 12:37:54 +0500

    test commit by dimash

--- projectSeptember2024/src/main/java/com/example/demo/DemoApplication.java
+++ projectSeptember2024/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//test commit
 public class DemoApplication {
 
 	public static void main(String[] args) {
