31332d04fb77316dbdfffe49e7b08e5081975c69
Medet Idrish <idrismedet0@gmail.com>
2024-09-09 12:08:46 +0500

    test comments are added

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+// this is my comment from IMedet
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
