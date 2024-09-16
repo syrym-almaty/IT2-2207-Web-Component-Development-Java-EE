8cd24e4090d2a874cbac71405b1ca81083d57562
syrym-almaty <s.zhakypbekov@iitu.edu.kz>
2024-09-16 12:18:32 +0500

    test

---
+++ demo/src/main/java/com/example/demo/repositories/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
