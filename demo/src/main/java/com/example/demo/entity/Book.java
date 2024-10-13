5fe0c89fc9384337e98d72063de62e11f59a95dc
Abdyrassulov_Dinmukhammet <abd944@mail.ru>
2024-10-13 22:38:58 +0500

    java: JDK isn't specified for module 'demo'. I will fix tomorrow

---
+++ demo/src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    // Constructors, Getters, and Setters
+}
