6f13f3a75a2150112c53c5743140c1e1fd9fc71d
Dauren <dau.ibraimov12@gmail.com>
2024-10-13 18:18:34 +0500

    Week 5 lab

--- demo/src/main/java/com/example/demo/controller/BookController.java
+++ demo/src/main/java/com/example/demo/controller/BookController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
 import com.example.demo.service.BookService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
@@ -15,18 +20,21 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @GetMapping
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
     @PostMapping
-    public Book createBook(@RequestBody Book book) {
+    public Book createBook(@Valid @RequestBody Book book) {
         return bookService.createBook(book);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable UUID id) {
+    public ResponseEntity<Book> getBookById(@Valid @PathVariable UUID id) {
         Book book = bookService.getBookById(id);
         if (book != null) {
             return ResponseEntity.ok(book);
@@ -35,7 +43,7 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable UUID id, @RequestBody Book bookDetails) {
+    public ResponseEntity<Book> updateBook(@PathVariable UUID id, @Valid @RequestBody Book bookDetails) {
         Book updatedBook = bookService.updateBook(id, bookDetails);
         if (updatedBook != null) {
             return ResponseEntity.ok(updatedBook);
@@ -45,7 +53,19 @@ public class BookController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable UUID id) {
-        bookService.deleteBook(id);
-        return ResponseEntity.noContent().build();
+        try {
+            bookService.deleteBook(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam String keyword) {
+        return bookRepository.findAll().stream()
+                .filter(book -> book.getTitle().contains(keyword) || book.getAuthor().contains(keyword))
+                .collect(Collectors.toList());
     }
 }
